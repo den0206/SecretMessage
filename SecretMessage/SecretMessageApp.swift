4d0b4aa40dc537112e14ad56e91b364757cb6054
den0206 <mac.pop@mineo.jp>
2021-03-19 22:43:44 +0900

    Initial Commit

---
+++ SecretMessage/SecretMessageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecretMessageApp.swift
+//  SecretMessage
+//
+//  Created by 酒井ゆうき on 2021/03/19.
+//
+
+import SwiftUI
+
+@main
+struct SecretMessageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
